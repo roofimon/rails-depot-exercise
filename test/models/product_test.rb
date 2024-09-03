3ae6889e47d2df799a641da7f8ee395553535cd2
Ruuf <ruuf@odds.team>
2024-09-03 16:24:31 +0700

    Load fixtures:

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  fixtures :products
   test "the truth" do
     assert true
   end
@@ -10,4 +11,17 @@ class ProductTest < ActiveSupport::TestCase
     assert product.errors[:title].any?
     assert true
   end
+  test "product price must be greater than 0.01" do
+    product = Product.new(title:        products(:one).title,
+                          description:  "Just a short description",
+                          image_url:    "zzz.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert ["must be greater than or equal to 0.01", product.errors[:price]]
+
+    product.price = 0
+    assert product.invalid?
+    assert ["must be greater than or equal to 0.01", product.errors[:price]]
+  end
+
 end
