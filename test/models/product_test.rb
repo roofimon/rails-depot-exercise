e5b0c428de299e31909a1ca1313938d6462d8d87
Ruuf <ruuf@odds.team>
2024-09-03 15:50:24 +0700

    Validate attributes with some basic tests for model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert true
+  end
 end
