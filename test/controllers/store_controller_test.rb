6beae3dc50b9f0b4da827fb8cdfde0b8000779f3
Ruuf <ruuf@odds.team>
2024-09-06 09:25:42 +0700

    Cache index

--- test/controllers/store_controller_test.rb
+++ test/controllers/store_controller_test.rb
@@ -4,5 +4,7 @@ class StoreControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
     get store_index_url
     assert_response :success
+    assert_select 'nav a', minimum: 4
+    assert_select 'h2', 'Programming Ruby 1.9'
   end
 end
