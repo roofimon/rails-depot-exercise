20959784b889aea0025efb75ecb027cc524e2a06
Ruuf <ruuf@odds.team>
2024-09-06 10:32:09 +0700

    Cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,17 @@
 class Product < ApplicationRecord
+    has_many :line_items
+    before_destroy :ensure_not_referenced_by_any_line_item
+
     validates :title, :description, :image_url, presence: true
     validates :title, uniqueness: true
     validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+    private 
+        def ensure_not_referenced_by_any_line_item
+            unless line_items.empty?
+                errors.add(:base, 'Line Items Present')
+                throw :abort
+            end 
+        end
+    
 end
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -60,4 +60,10 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
     assert_redirected_to products_url
   end
+
+  test "can't delete product in cart" do
+    assert_difference("Product.count", 0) do
+      delete product_url(products(:two))
+    end
+  end
 end
