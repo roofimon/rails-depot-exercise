e660f0422ae44447d88d6d412c90b424c0345f17
Ruuf <ruuf@odds.team>
2024-09-07 20:59:09 +0700

    View and controller for quantity

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -24,14 +24,18 @@ class LineItemsController < ApplicationController
   # POST /line_items or /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to cart_url(@line_item.cart), notice: "Line item was successfully created." }
-        format.json { render :show, status: :created, location: @line_item }
+        format.html { redirect_to cart_url(@line_item.cart), 
+          notice: "Line item was successfully created." }
+        format.json { render :show, 
+          status: :created, location: @line_item }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :new, 
+          status: :unprocessable_entity }
+        format.json { render json: @line_item.errors, 
+          status: :unprocessable_entity }
       end
     end
   end
